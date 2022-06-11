fe8315b8fc2512c94b8acebfbdd8e81696ba13f3
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-06-11 14:37:19 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+import { fabric } from "fabric";
+const App = () => {
+  const [canvas, setCanvas] = useState('');  
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);  
+  
+  const initCanvas = () => (
+    new fabric.Canvas('canvas', {
+      height: 750,
+      width: 1600,
+      backgroundColor: 'pink'
+    })
+  ); 
+   const addRect = canvi => {
+     const rect = new fabric.Rect({
+       height: 280,
+       width: 200,
+       fill: 'yellow'
+     });
+     canvi.add(rect);
+     canvi.renderAll();
+   }
+ 
   return (
-    <div className="App">
-     heelo
+    <div>
+      <h1>Fabric.js on React - fabric.Canvas</h1>
+      <button onClick={() => addRect(canvas)}>Rectangle</button>
+      <br />
+      <br />
+      <canvas id="canvas" />
     </div>
   );
-}
+};
 
 export default App;
